289a5c78fcb11343420090f25fedcf19c268e567
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-06-12 23:39:07 +0100

    Still cooking

--- function.js
+++ function.js
@@ -1,3 +1,6 @@
 const expenseForm = document.querySelector("#expense-form");
 const date = document.querySelector("#date");
 const category = document.querySelector("#category");
+const description = document.querySelector("#description");
+const amount = document.querySelector("#amount");
+const expenseTable = document.querySelector;

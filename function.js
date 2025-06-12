22a061744871df3fc1acb81850f7c17756a6079b
Isyak Abdulrasheed <abdulrasheedisyak44@gmail.com>
2025-06-12 23:42:15 +0100

    Still cooking

--- function.js
+++ function.js
@@ -4,4 +4,5 @@ const category = document.querySelector("#category");
 const description = document.querySelector("#description");
 const amount = document.querySelector("#amount");
 const expenseTable = document.querySelector("#expense-table-body");
-const totalContainer = document.querySelector;
+const totalContainer = document.querySelector("#total-container");
+const totalAmount = document.querySelector("#");
